6871e65fda42d8af71a5743d4fada1774edb372b
lisns <z.z.iz.0pnup@gmail.com>
2019-05-21 20:28:38 +0900

    プロジェクト名変更と新規プロジェクト作成

---
+++ 503_TextLog/src/ofApp.cpp
@@ -0,0 +1,91 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+    // 背景色グレー
+    ofSetBackgroundColor(40, 40, 40);
+    // 600x400 30 frames/sec のウィンドウ
+    ofSetWindowShape(600, 400);
+    ofSetFrameRate(30);
+
+    // 初期位置
+    posX = 300;
+    posY = 200;
+    goalX = posX;
+    goalY = posY;
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+    // マウス動作
+    float difX, difY;
+    difX = goalX - posX;
+    difY = goalY - posY;
+    // 丸の位置
+    posX += difX * 0.05;
+    posY += difY * 0.05;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+    ofSetColor(215, 19, 69);
+    ofDrawCircle(posX, posY, 10);
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+    goalX = x;
+    goalY = y;
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+    goalX = x;
+    goalY - y;
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
